62054292e6e1bdb02c46787ad36da31e21ba6bc5
2h1082 <hongil0824@naver.com>
2025-01-02 15:54:13 +0900

    Assignment3 complete

---
+++ SimpleVector.h
@@ -0,0 +1,34 @@
+template <typename T>
+class SimpleVector{
+private:
+    T* data;
+    int currentSize;
+    int currentCapacity;
+public:
+    void resize(int newCapacity){
+
+    }
+    SimpleVector(){}
+    SimpleVector(int capacity){
+
+    }
+    SimpleVector(const SimpleVector& other){
+
+    }
+    ~SimpleVector(){}
+    void push_back(const T& value){
+
+    }
+    void pop_back(){
+
+    }
+    int size(){
+
+    }
+    int capacity(){
+
+    }
+    void SortData(){
+
+    }
+};
