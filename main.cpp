00988b657c36388eec4ea63ae44bc33fe326c73a
2h1082 <hongil0824@naver.com>
2025-01-02 20:21:19 +0900

    fix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#endif
 #include "SimpleVector.h"
 using namespace std;
 
 int main(){
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     SimpleVector<int> v;
     v.push_back(10);
     v.push_back(30);
