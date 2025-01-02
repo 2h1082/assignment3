270b9eda863795571e7e7d469a1ef28c84817f0a
2h1082 <hongil0824@naver.com>
2025-01-02 15:56:01 +0900

    fix

--- SimpleVector.h
+++ SimpleVector.h
@@ -1,3 +1,11 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
 template <typename T>
 class SimpleVector{
 private:
@@ -6,29 +14,54 @@ private:
     int currentCapacity;
 public:
     void resize(int newCapacity){
-
+        if(newCapacity>currentCapacity){
+            T* temp=new T[newCapacity]; //resized array
+            memcpy(temp,data,currentSize*sizeof(T)); //copy
+            delete[] data; //delete original array
+            data=temp;
+            currentCapacity=newCapacity;
+        }
     }
-    SimpleVector(){}
-    SimpleVector(int capacity){
-
+    SimpleVector(int capacity=10){
+        data=new T[capacity];
+        currentSize=0;
+        currentCapacity=capacity;
     }
     SimpleVector(const SimpleVector& other){
-
+        this->data=new T[other.size()];
+        this->currentCapacity=other.capacity();
+        this->currentSize=other.size();
+        memcpy(this->data,other.data,other.size()*sizeof(T));
     }
-    ~SimpleVector(){}
+    ~SimpleVector(){ 
+        delete[] data;
+        _CrtDumpMemoryLeaks(); //check memory leaks
+    }
     void push_back(const T& value){
-
+        if(currentSize==currentCapacity){ //Full
+            resize(currentCapacity+5);
+        }
+        data[currentSize++]=value;
     }
     void pop_back(){
-
+        if(currentSize>0){
+            data[--currentSize]=T(); //default value(delete original value)
+        }
     }
-    int size(){
-
+    int size() const {
+        return currentSize;
     }
-    int capacity(){
-
+    int capacity() const {
+        return currentCapacity;
+    }
+    void printData(){
+        cout<<"{ ";
+        for(int i=0;i<currentSize;i++){
+            cout<<data[i]<<" ";
+        }
+        cout<<"}"<<endl;
     }
     void SortData(){
-
+        sort(data,data+currentSize);
     }
 };
---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "SimpleVector.h"
+using namespace std;
+
+int main(){
+    SimpleVector<int> v;
+    v.push_back(10);
+    v.push_back(30);
+    v.push_back(40);
+    cout<<v.size()<<" "<<v.capacity()<<endl;
+    cout<<"v(int vector): ";
+    v.printData();
+    v.pop_back();
+    v.pop_back();
+    cout<<v.size()<<" "<<v.capacity()<<endl;
+    cout<<"v(int vector): ";
+    v.printData();
+    v.resize(15);
+    cout<<"resized v: ";
+    cout<<v.size()<<" "<<v.capacity()<<endl;
+    
+    SimpleVector<char> s(5);
+    s.push_back('c');
+    s.push_back('b');
+    s.push_back('z');
+    s.push_back('h');
+    s.push_back('s');
+    s.push_back('e');
+    cout<<"s(char vector): ";
+    s.printData();
+    s.SortData();
+    cout<<"sorted s: ";
+    s.printData();
+    cout<<s.size()<<" "<<s.capacity()<<endl;
+
+    SimpleVector<char> dup(s);
+    cout<<"dup(duplicated vector): ";
+    dup.printData();
+    cout<<dup.size()<<" "<<dup.capacity()<<endl;
+    dup.pop_back();
+    dup.pop_back();
+    dup.pop_back();
+    cout<<"updated dup(pop_back): ";
+    dup.printData();
+    cout<<dup.size()<<" "<<dup.capacity()<<endl;
+    return 0;
+}
